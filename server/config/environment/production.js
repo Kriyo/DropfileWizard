db1a4e63e7a2bc15747a282af7c6a00cf1366207
Chris Kennedy <krikenn@gmail.com>
2015-10-08 11:51:01 +0100

    ip test

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,8 +5,7 @@
 module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+            process.env.IP || '0.0.0.0',
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
